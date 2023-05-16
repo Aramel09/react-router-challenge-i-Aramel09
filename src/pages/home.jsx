8a91b07266fe5f874fab5b17d3d40116f4d71d3c
Alejandro Melara <alejandro.melara96@gmail.com>
2023-05-15 23:07:57 -0400

    Created Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,22 @@
-import { information } from "./loaders";
-import { useAsyncValue } from "react-router-dom";
-import Thought from "./thought";
+import { Suspense } from "react";
+import { Await, useLoaderData } from "react-router-dom";
+import List from "../users/user-list";
 
 export default function Home() {
-  const items = useAsyncValue();
+  const items = useLoaderData();
+  const itemsUsers = items.users;
+
+  console.log("Sin corchetes", items);
+  console.log("Con corchetes", { items });
+  console.log("Con punto", items.users);
+
   return (
     <>
-      <ol className="list-decimal space-y-4">
-        {items.map((thought) => (
-          <Thought key={thought.id} thought={thought} />
-        ))}
-      </ol>
+      <Suspense>
+        <Await resolve={itemsUsers}>
+          <List />
+        </Await>
+      </Suspense>
     </>
   );
 }
