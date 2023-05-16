091b3187a6d900f95811213827f4669b526167c5
Alejandro Melara <alejandro.melara96@gmail.com>
2023-05-15 23:09:54 -0400

    Created user list that shows on Home page

--- src/users/user-list.jsx
+++ src/users/user-list.jsx
@@ -1,13 +1,13 @@
 import { useAsyncValue } from "react-router-dom";
-import User from "./user";
 
-export default function Home() {
-  const items = useAsyncValue();
+export default function List() {
+  const list = useAsyncValue();
+
   return (
     <>
       <ol className="list-decimal space-y-4">
-        {items.map((users) => (
-          <User key={users.id} users={name} />
+        {list.map((users) => (
+          <li key={users.id}>{users.name}</li>
         ))}
       </ol>
     </>
