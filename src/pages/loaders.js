eeccd9aab98afb90b8baedc3fab1018a6fc3d1c5
Alejandro Melara <alejandro.melara96@gmail.com>
2023-05-15 23:09:01 -0400

    Created loader for the data in db.json
    I created a route for "ky" and one for "fetch"

--- src/pages/loaders.js
+++ src/pages/loaders.js
@@ -1,8 +1,13 @@
 import { defer } from "react-router-dom";
-import apiServices from "../services/api";
+// import apiServices from "../services/api";
+import apiService from "../api.service";
 
 export const information = () => {
-  const users = apiServices.retrieveInfo();
+  //   const users = apiServices.retrieveInfo();
 
+  //   return defer({ users });
+
+  const users = apiService.index();
+  console.log({ users });
   return defer({ users });
 };
