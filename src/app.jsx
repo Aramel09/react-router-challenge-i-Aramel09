3230edf7545f838da7e05069451aafb0553422a8
Alejandro Melara <alejandro.melara96@gmail.com>
2023-05-15 23:06:52 -0400

    Created react router Home route

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,23 @@
-import loader from "./pages/loaders";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/home";
+import { information } from "./pages/loaders";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />} loader={information} />
+  )
+);
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div>{loader}</div>
+      <RouterProvider router={router} />
     </>
   );
 }
