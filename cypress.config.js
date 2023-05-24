dc79358aefadc5161a658138368cc82c2f961a3f
Alejandro Melara <alejandro.melara96@gmail.com>
2023-05-23 20:52:13 -0400

    Downloaded cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "cypress";
-import vitePreprocessor from "cypress-vite";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on) {
-      on("file:preprocessor", vitePreprocessor());
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
-    viteConfig,
   },
 });
