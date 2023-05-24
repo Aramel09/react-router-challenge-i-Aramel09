0df38a980ae967965566b8a98b2030a81d92e815
Alejandro Melara <alejandro.melara96@gmail.com>
2023-05-23 20:56:24 -0400

    Connecting Vite and Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+import viteConfig from "./vite.config";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on("file:preprocessor", vitePreprocessor());
       // implement node event listeners here
     },
+    viteConfig,
   },
 });
