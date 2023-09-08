4e92454bba4075860ccbec4a2e8bf807327c0373
Alejandro Melara <alejandro.melara96@gmail.com>
2023-09-08 18:33:38 -0400

    docs: :memo: Add thoughts for the CY tests

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -5,6 +5,7 @@ export default function Loading() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
+      data-cy="loading"
     >
       <circle
         className="opacity-25"
--- src/users/user-list.jsx
+++ src/users/user-list.jsx
@@ -2,10 +2,11 @@ import { Link, useAsyncValue } from "react-router-dom";
 
 export default function List() {
   const list = useAsyncValue();
+  console.log(list);
 
   return (
     <>
-      <ol className="list-decimal space-y-4">
+      <ol className="list-decimal space-y-4" data-cy="">
         {list.map((users) => (
           <li key={users.id}>
             <Link to={`/userProfile/${users.id}`}>{users.name}</Link>
