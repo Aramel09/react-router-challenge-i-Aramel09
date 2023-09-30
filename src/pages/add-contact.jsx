b5ce06ec71fa3a5cbe75a2ec627e10f852f0a9e5
Alejandro Melara <alejandro.melara96@gmail.com>
2023-09-30 10:26:48 -0400

    feat: Add defaultValue, id, name to be able to Edit information

--- src/pages/add-contact.jsx
+++ src/pages/add-contact.jsx
@@ -1,15 +1,35 @@
-import { Form } from "react-router-dom";
+import { Form, useLocation } from "react-router-dom";
 
 export default function AddContact() {
+  let { state } = useLocation();
+  console.log(state);
+
   return (
     <>
-      <Form method="post">
+      <Form method={state ? "PATCH" : "POST"}>
         <div className="flex w-2/5 flex-col space-y-5">
-          <input className="text-black" id="name" name="name" />
-          <input className="text-black" id="email" name="email" />
-          <input className="text-black" id="tel" name="tel" />
+          <input
+            className="text-black"
+            id="name"
+            name="name"
+            defaultValue={state ? state.users.name : ""}
+          />
+          <input
+            className="text-black"
+            id="email"
+            name="email"
+            defaultValue={state ? state.users.email : ""}
+          />
+          <input
+            className="text-black"
+            id="tel"
+            name="tel"
+            defaultValue={state ? state.users.tel : ""}
+          />
+          <input type="hidden" name="id" value={state ? state.users.id : ""} />
+
           <button type="submit" className="btn">
-            Add
+            {state ? "Edit" : "Add"}
           </button>
         </div>
       </Form>
