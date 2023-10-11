74f370e2eca41cd834b2e5adbce1260564a4b37e
Alejandro Melara <alejandro.melara96@gmail.com>
2023-10-10 22:02:35 -0400

    style: :lipstick: Space evenly nav bar, add btn css and centered headers

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import NavBar from "./nav-bar";
 
 export default function Header() {
   return (
-    <header className="items-center justify-between sm:flex sm:flex-row sm:pl-8">
+    <header>
       <NavBar />
     </header>
   );
--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <>
-      <nav className="border-b-2">
-        <ul className="divide-y bg-slate-700 px-4 sm:flex sm:gap-x-16 sm:divide-y-0 [&>*]:py-4">
+      <nav>
+        <ul className="flex justify-evenly">
           <Link to={"/"}>
             <h1 className="py-4 text-2xl font-bold">Contacts</h1>
           </Link>
           <Link to={"/Add-Contact"}>
-            <h1>Add contact</h1>
+            <h1 className="btn m-3">Add contact</h1>
           </Link>
         </ul>
       </nav>
