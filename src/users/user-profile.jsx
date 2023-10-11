61ad3730c02a4cd4c27c9d65cabcb0a0e4f0ee9c
Alejandro Melara <alejandro.melara96@gmail.com>
2023-10-10 22:49:38 -0400

    style: :lipstick: Center items, add red color to button and deleted ID user profile
    User ID was unnecesary to show

--- src/users/user-profile.jsx
+++ src/users/user-profile.jsx
@@ -8,36 +8,41 @@ export default function UserProfile() {
 
   return (
     <>
-      <img src={users.img} alt="persons face"></img>
-      <li className="font-medium">
-        {users.name}
-        <small className="block space-x-2 italic">
-          <span>{users.id}</span>
-          <span>{users.email}</span>
-          <span>{users.tel}</span>
-        </small>
-      </li>
-      <Link to={"/"} className="btn">
-        Back
-      </Link>
-      <Link to={"/Add-Contact"} className="btn" state={{ users }}>
-        Edit
-      </Link>
-      <button
-        className="btn"
-        onClick={() => {
-          submit(
-            {
-              id: users.id,
-            },
-            {
-              method: "DELETE",
-            }
-          );
-        }}
-      >
-        Delete
-      </button>
+      <div className="flex flex-col items-center">
+        <div className="my-2">
+          <img src={users.img} alt="persons face"></img>
+        </div>
+        <div className="my-2 ">
+          <li className="list-none text-center font-medium">
+            {users.name}
+            <small className="my-1 flex flex-col justify-center space-x-2 italic">
+              <span className="text-gray-500">{users.email}</span>
+              <span className=" text-gray-500">{users.tel}</span>
+            </small>
+          </li>
+        </div>
+        <Link to={"/"} className="btn my-2">
+          Back
+        </Link>
+        <Link to={"/Add-Contact"} className="btn my-2" state={{ users }}>
+          Edit
+        </Link>
+        <button
+          className="btn my-2 bg-red-600"
+          onClick={() => {
+            submit(
+              {
+                id: users.id,
+              },
+              {
+                method: "DELETE",
+              }
+            );
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </>
   );
 }
