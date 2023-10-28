6e531c489054cc8e2e90df45c74e6ba37fd19c20
Alejandro Melara <alejandro.melara96@gmail.com>
2023-10-28 12:27:44 -0400

    chore: :wrench: fix ESlint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
-    "plugin: cypress/recommended",
+    "plugin:cypress/recommended",
     "standard",
     "prettier",
   ],
