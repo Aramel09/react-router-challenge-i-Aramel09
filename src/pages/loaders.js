aea7d44891d59c56d7cba9eec68e06a2119a589b
Alejandro Melara <alejandro.melara96@gmail.com>
2023-11-09 20:05:52 -0500

    refactor: Remove a console log

--- src/pages/loaders.js
+++ src/pages/loaders.js
@@ -4,8 +4,6 @@ import apiService from "../api.service";
 export const information = ({ params }) => {
   const { id } = params;
 
-  console.log(id);
-
   const users = id ? apiService.show(id) : apiService.index();
   return defer({ users });
 };
